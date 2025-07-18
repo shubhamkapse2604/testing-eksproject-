ff31a4ee3a6345cdd5c98fe3c15181e6ad43e3c1
shubhamkapse2604 <140498733+shubhamkapse2604@users.noreply.github.com>
2025-07-18 19:32:08 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,8 +1,8 @@
 // const API_BASE_URL = "http://3.87.228.149:84";
-const API_BASE_URL = "http://api.sk2604cloud.shop";
-export default API_BASE_URL;
+//const API_BASE_URL = "http://api.sk2604cloud.shop";
+//export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 // export default API_BASE_URL;
-//const API_BASE_URL = "REACT_APP_API_BASE_URL_PLACEHOLDER";
-//export default API_BASE_URL;
+const API_BASE_URL = "a745866107f944aae89af01569e2d29d-307274974.us-east-1.elb.amazonaws.com";
+export default API_BASE_URL;
 
